d1277c03b5290a61f42d1de5308199384e571528
Joshua Chao <joshua.chao39@gmail.com>
2017-02-07 22:04:15 -0800

    forgot to include index.js

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MobileContainer from './components/MobileContainer'
+import DesktopContainer from './components/DesktopContainer'
+import $ from 'jquery';
+import './styles/index.css';
+
+import Firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyCF_-IUfnv4rTfQncPz-9Ja6gcBnaRSSSo",
+    authDomain: "queuemmunal.firebaseapp.com",
+    databaseURL: "https://queuemmunal.firebaseio.com",
+    storageBucket: "queuemmunal.appspot.com",
+    messagingSenderId: "376397752355"
+  };
+
+ Firebase.initializeApp(config);
+
+ let App = React.createClass({
+ 	render() {
+    if (this.state.width > 480 || this.state.width > this.state.height) {
+      return (
+        <div className="flexContainer">
+          <DesktopContainer />
+        </div>
+      );
+    } else {
+      return (
+        <MobileContainer />
+      );
+    }
+  },
+  updateDimensions(){
+    this.setState({
+      width: $(window).width(),
+      height: $(window).height()
+    });
+  },
+  componentWillMount() {
+    this.updateDimensions();
+  },
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+  },
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+ });
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
