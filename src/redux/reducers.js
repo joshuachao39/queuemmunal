10e31176ee787cb18f048f5754e596ad1df37c7d
Ajeya Rengarajan <ajrengar@ucsd.edu>
2017-02-21 22:49:00 -0800

    redux setup

---
+++ src/redux/reducers.js
@@ -0,0 +1,44 @@
+/**
+* state of the app
+* loggedIn: true if user is logged in, false otherwise
+* currentRoom: name of the room the user is in
+*/
+
+const initialState = {
+    loggedIn : false,
+    currentRoom : undefined
+};
+
+
+function appReducers (state, action) {
+
+    if (typeof state === 'undefined') {
+        return initialState;
+    }
+
+    switch (action.type) {
+
+    case LOG_OUT: {
+        return Object.assign ({}, state, {
+            loggedIn: false
+        });
+    }
+
+    case LOG_IN: {
+        return Object.assign ({}, state, {
+            loggedIn: true
+        });
+    }
+
+    case UPDATE_ROOM: {
+        return Object.assign ({}, state, {
+            currentCourse: action.room
+        });
+    }
+
+    }
+
+    return state;
+}
+
+export default appReducers;
